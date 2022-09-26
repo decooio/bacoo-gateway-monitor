02f31ca391806b7867751a53c366fb6983928474
gavine <gavinegaowen@gmail.com>
2022-09-26 17:12:07 +0800

    adjust cron

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import {logger} from './logger';
 import {unpinLocalFiles} from './service/index';
 
 
-schedule.scheduleJob('0/5 * * * * *', async () => {
+schedule.scheduleJob('0 0 */1 * * ?', async () => {
   logger.info('unpin schedule start');
   await unpinLocalFiles()
   logger.info('unpin schedule end');
